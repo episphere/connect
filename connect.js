65160714a80b1b4e2b6c37089e5a9672a54af476
Jonas Almeida <jalmeida@mathbiol.org>
2019-02-27 20:37:18 -0500

    it's alive

--- connect.js
+++ connect.js
@@ -1,1 +1,18 @@
 console.log('connect.js loaded')
+
+cnct=function(){
+    // ini
+    console.log('ini at '+Date())
+    let div = document.body.querySelector('#connectDiv')
+    if(div){cnct.UI(div)}
+}
+
+cnct.UI=function(div){
+    let h = '<h3>API status</h3>'
+    h += '<input size=50> <button>Send</button>'
+    h += '<textarea stylw="width:500px"></textarea>'
+    
+    div.innerHTML=h
+}
+
+window.onload=cnct
