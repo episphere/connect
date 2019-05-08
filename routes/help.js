1dc1a9d419b17c05e37168cde6c354ae939570a9
PrafulB <prtsh32@gmail.com>
2019-05-08 17:15:53 -0400

    Added /case endpoint to help response

--- routes/help.js
+++ routes/help.js
@@ -5,6 +5,7 @@ module.exports.helpHandler = (ctx) => {
         '/files : lists files under your API key (provided as header) <-- PENDING YOUR FEEDBACK',
         '/files/<submission_id> : returns file <-- PENDING YOUR FEEDBACK',
         '/files/<submission_id>/<case_id> : returns file row for corresponding case <-- PENDING YOUR FEEDBACK',
+        '/case/<case_id> : returns a single record'
         '/info/ : returns summary information on your files <-- PENDING YOUR FEEDBACK',
         '/info/<submission_id> : returns detailed information on file <-- PENDING YOUR FEEDBACK',
         '/transactions> : log of transactions using your key <-- PENDING YOUR FEEDBACK',
